4889566099bce3adbda78068c27a49e1cdf805b7
sungwoo-shin <efilevol42@gmail.com>
2023-10-22 12:33:04 +0900

    queue

---
+++ data-structures/queue.js
@@ -0,0 +1,29 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIdx = 0;
+    this.tailIdx = 0;
+  }
+
+  enqueue(item) {
+    this.items[this.tailIdx] = item;
+    this.tailIdx += 1;
+  }
+
+  dequeue() {
+    const headItem = this.items[this.headIdx];
+    delete this.items[this.headIdx];
+    this.headIdx += 1;
+    return headItem;
+  }
+
+  peek() {
+    return this.items[this.headIdx];
+  }
+
+  getLength() {
+    return this.tailIdx - this.headIdx;
+  }
+}
+
+module.exports = Queue;
